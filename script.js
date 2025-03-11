516012178d4ee7a25777995ca800f6c27904e31d
rafasantossss <rafael_santos32@estudante.sesisenai.org.br>
2025-03-11 19:34:37 -0300

    v2

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ const personagem = {
     y: chaoY - 150,
     largura: 110,
     altura: 150,
-    velocidadeX: 0,  // Velocidade horizontal do personagem
-    aceleracao: 0.5, // Aceleração para quando o personagem anda
-    desaceleracao: 0.89 , // Fator de desaceleração para quando a tecla é solta
+    velocidadeX: 0,
+    aceleracao: 0.5,
+    desaceleracao: 0.5,
     velocidadey: 0,
     pulando: false,
     larguraPulo: 115,
-    alturaPulo: 120
+    alturaPulo: 120,
+    imagemAndando: 0,
+    imagemPadrao: true,
 };
 
 const obstaculo = {
@@ -22,10 +24,18 @@ const obstaculo = {
     y: chaoY - 150,
     largura: 100,
     altura: 150,
-    velocidade: 1
+    velocidade: Math.random() * (6 - 3) + 3 
 };
 
-const gravidade = 0.13;  // Gravidade suave
+const lois = {
+    x: 1200,
+    y: chaoY - 150,
+    largura: 150,
+    altura: 150,
+    velocidade: Math.random() * (6 - 3) + 3  
+};
+
+const gravidade = 0.5;
 
 const imgMorte = new Image();
 imgMorte.src = 'morte.jpg';
@@ -42,23 +52,35 @@ imgPersonagemPulo.src = 'imagempulo.webp';
 const imgFundo = new Image();
 imgFundo.src = 'imagem.jpg';
 
+const imgLois = new Image();
+imgLois.src = 'lois.png';
+
+const imgPersonagemAndando = [
+    new Image(),
+    new Image(),
+    new Image(),
+];
+imgPersonagemAndando[0].src = '1.png';
+imgPersonagemAndando[1].src = '2.png';
+imgPersonagemAndando[2].src = '3.png';
+
 let gameOver = false;
 
-// Controla as teclas pressionadas
 let teclas = {
     esquerda: false,
     direita: false,
     espaco: false
 };
 
-// Para controle de FPS
 const FPS = 60;
 let lastFrameTime = 0;
 let deltaTime = 0;
 
+let velocidadeQueda = 0;
+
 document.addEventListener('keydown', (e) => {
     if (e.code === 'Space' && !personagem.pulando) {
-        personagem.velocidadey = 8;  // Velocidade do pulo
+        personagem.velocidadey = 15;
         personagem.pulando = true;
     }
     if (e.code === 'KeyD') {
@@ -69,6 +91,17 @@ document.addEventListener('keydown', (e) => {
     }
 });
 
+document.addEventListener('keydown', (e) => {
+    if (e.code === 'KeyS') {
+        velocidadeQueda = 5;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    if (e.code === 'KeyS') {
+        velocidadeQueda = 0;
+    }
+});
 document.addEventListener('keyup', (e) => {
     if (e.code === 'KeyD') {
         teclas.direita = false;
@@ -78,45 +111,53 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
-// Função para desenhar o personagem
 function desenharPersonagem() {
+    ctx.save();
+
     if (personagem.pulando) {
-        ctx.drawImage(imgPersonagemPulo, personagem.x, personagem.y, personagem.larguraPulo, personagem.alturaPulo);
-    } else {
-        // Se estiver se movendo para a esquerda, inverte a imagem
-        if (teclas.esquerda) {
-            ctx.save(); // Salva o estado do contexto
-            ctx.scale(-1, 1); // Inverte horizontalmente
-            ctx.drawImage(imgPersonagem, -personagem.x - personagem.largura, personagem.y, personagem.largura, personagem.altura);
-            ctx.restore(); // Restaura o estado original do contexto
+        if(teclas.esquerda){
+            ctx.scale(-1, 1);
+            ctx.drawImage(imgPersonagemPulo, -personagem.x - personagem.largura, personagem.y, personagem.largura, personagem.altura);
+            ctx.restore();
         } else {
+            ctx.drawImage(imgPersonagemPulo, personagem.x, personagem.y, personagem.largura, personagem.altura);
+        }
+    } else {
+        if (teclas.direita || teclas.esquerda) {
+            // Se o personagem está andando, alterna entre as imagens de caminhada
+            const imagemAndando = imgPersonagemAndando[Math.floor(Date.now() / 100) % 3];
+
+            if (teclas.esquerda) {
+                ctx.scale(-1, 1);
+                ctx.drawImage(imagemAndando, -personagem.x - personagem.largura, personagem.y, personagem.largura, personagem.altura);
+                ctx.restore();
+            } else {
+                ctx.drawImage(imagemAndando, personagem.x, personagem.y, personagem.largura, personagem.altura);
+            }
+        } else {
+            // Se o personagem não está andando, usa a imagem padrão
             ctx.drawImage(imgPersonagem, personagem.x, personagem.y, personagem.largura, personagem.altura);
         }
     }
 }
 
-// Função para atualizar a física do personagem
 function atualizarPersonagem() {
-    // Movimento horizontal suave
     if (teclas.direita) {
-        personagem.velocidadeX += personagem.aceleracao;  // Aumenta a velocidade para a direita
+        personagem.velocidadeX += personagem.aceleracao;
     } else if (teclas.esquerda) {
-        personagem.velocidadeX -= personagem.aceleracao;  // Aumenta a velocidade para a esquerda
+        personagem.velocidadeX -= personagem.aceleracao;
     } else {
-        // Desacelera se nenhuma tecla horizontal for pressionada
         personagem.velocidadeX *= personagem.desaceleracao;
     }
 
-    // Limita a velocidade máxima
-    if (personagem.velocidadeX > 2) personagem.velocidadeX = 2;
-    if (personagem.velocidadeX < -2) personagem.velocidadeX = -2;
+    if (personagem.velocidadeX > 5) personagem.velocidadeX = 5;
+    if (personagem.velocidadeX < -5) personagem.velocidadeX = -5;
 
-    // Atualiza a posição do personagem horizontalmente
     personagem.x += personagem.velocidadeX;
 
-    // Movimento vertical (gravidade e pulo)
     if (personagem.pulando) {
         personagem.velocidadey -= gravidade;
+        personagem.velocidadey -= velocidadeQueda;
         personagem.y -= personagem.velocidadey;
 
         if (personagem.y >= chaoY - personagem.altura) {
@@ -127,41 +168,64 @@ function atualizarPersonagem() {
     }
 }
 
-// Função para desenhar o obstaculo
 function desenharObstaculo() {
     ctx.drawImage(imgInimigo, obstaculo.x, obstaculo.y, obstaculo.largura, obstaculo.altura);
 }
 
-// Função para atualizar o movimento do obstaculo
 function atualizarObstaculo() {
     obstaculo.x -= obstaculo.velocidade;
-
     if (obstaculo.x <= 0 - obstaculo.largura) {
         obstaculo.x = canvas.width;
-        obstaculo.velocidade += 0.2;  // Aceleração mais lenta
+        obstaculo.velocidade += Math.random() * (0.4 - 0.2) + 0.1;
     }
+
+    if (obstaculo.velocidade > 12) obstaculo.velocidade = 12;
 }
 
-// Função para verificar colisão
+function desenharLois() {
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.drawImage(imgLois, -lois.x - lois.largura, lois.y, lois.largura, lois.altura);
+    ctx.restore();
+}
+
+function atualizarLois() {
+    lois.x -= lois.velocidade;
+    if (lois.x <= 0 - lois.largura) {
+        lois.x = canvas.width;
+        lois.velocidade += Math.random() * (0.6 - 0.3) + 0.1;
+    }
+
+    if (lois.velocidade > 12) lois.velocidade = 12;
+}
+
 function verificarColisão() {
+    const margem = 25;
     if (
-        personagem.x < obstaculo.x + obstaculo.largura &&
-        personagem.x + personagem.largura > obstaculo.x &&
-        personagem.y < obstaculo.y + obstaculo.altura &&
-        personagem.y + personagem.altura > obstaculo.y
+        personagem.x + margem < obstaculo.x + obstaculo.largura - margem &&
+        personagem.x + personagem.largura - margem > obstaculo.x + margem &&
+        personagem.y + margem < obstaculo.y + obstaculo.altura - margem &&
+        personagem.y + personagem.altura - margem > obstaculo.y + margem
+    ) {
+        gameOver = true;
+    }
+    if (
+        personagem.x + margem < lois.x + lois.largura - margem &&
+        personagem.x + personagem.largura - margem > lois.x + margem &&
+        personagem.y + margem < lois.y + lois.altura - margem &&
+        personagem.y + personagem.altura - margem > lois.y + margem
     ) {
         gameOver = true;
     }
 }
 
-function fundo (){
+function fundo() {
     ctx.drawImage(imgFundo, 0, 0, canvas.width, canvas.height);
 }
-// Função principal do loop de animação
-function loop(timestamp) {
-    deltaTime = timestamp - lastFrameTime;
-    // Só executa o loop se o tempo entre os frames for suficiente para manter o FPS constante
-    if (deltaTime >= 100 / FPS) {
+
+function loop() {
+    deltaTime = performance.now() - lastFrameTime;
+    if (deltaTime >= 500 / FPS) {
         if (!gameOver) {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             fundo();
@@ -170,40 +234,40 @@ function loop(timestamp) {
             desenharObstaculo();
             atualizarObstaculo();
             atualizarPersonagem();
+            desenharLois();
+            atualizarLois();
 
-            lastFrameTime = timestamp; // Atualiza o tempo do último quadro
+            lastFrameTime = performance.now();
         } else {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             ctx.drawImage(imgMorte, 0, 0, canvas.width, canvas.height);
             ctx.font = '40px Arial';
             ctx.fillStyle = 'white';
-            ctx.fillText('Você morreu! Clique para reiniciar', 100, canvas.height / 2);
+            ctx.fillText('Você morreu!', 100, canvas.height / 2);
         }
     }
-    requestAnimationFrame(loop);  // Continuar chamando o loop
+    requestAnimationFrame(loop);
 }
 
-// Função para reiniciar o jogo
 function reiniciarJogo() {
     if (gameOver) {
-        // Resetando as variáveis
         gameOver = false;
         personagem.x = 100;
         personagem.y = chaoY - 150;
         personagem.velocidadey = 0;
         personagem.pulando = false;
         obstaculo.x = 900;
-        obstaculo.velocidade = 1;
-        loop(performance.now());  
+        obstaculo.velocidade = Math.random() * (6 - 3) + 3; 
+        lois.velocidade = Math.random() * (6 - 3) + 3; 
+        lois.x = 1200;
+        loop(performance.now());
     }
 }
 
-// Adiciona evento de clique para reiniciar o jogo
 canvas.addEventListener('click', reiniciarJogo);
 
-// Aguardar as imagens carregarem e só iniciar o jogo depois do clique
-imgPersonagem.onload = imgInimigo.onload = imgPersonagemPulo.onload = () => {
+imgPersonagem.onload = imgInimigo.onload = imgPersonagemPulo.onload = imgLois.onload = () => {
     canvas.addEventListener('click', () => {
-        loop(performance.now());  // Inicia o jogo apenas após o clique
+        loop(performance.now());
     });
 };
